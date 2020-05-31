43a79da1fa07ab9591bff4c7c98f57aa9e4f6e8d
semioys <semioys@gmail.com>
2020-05-31 23:05:39 +0300

    Final styles were applied

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -17,6 +17,7 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Monsters Rolodex</h1>
       <SearchBox placeholder={'Search monsters'} handleChange={(e) => setsearchField(e.target.value)} />
       <CardList cards={filteredMonsters} />
     </div>
